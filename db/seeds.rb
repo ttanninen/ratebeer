6b0693a5289b08c25fa2ee0f8bfdc5b61400b406
ttanninen <teemu@TEPU>
2025-02-16 22:25:23 +0200

    Tehtävään 6 asti, eli seuraavaksi aletaan lisäämään yksittäisiä oluita

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,15 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+b1 = Brewery.create name: "Koff", year: 1897
+b2 = Brewery.create name: "Malmgard", year: 2001
+b3 = Brewery.create name: "Weihenstephaner", year: 1040
+
+b1.beers.create name: "Iso 3", style: "Lager"
+b1.beers.create name: "Karhu", style: "Lager"
+b1.beers.create name: "Tuplahumala", style: "Lager"
+b2.beers.create name: "Huvila Pale Ale", style: "Pale Ale"
+b2.beers.create name: "X Porter", style: "Porter"
+b3.beers.create name: "Hefeweizen", style: "Weizen"
+b3.beers.create name: "Helles", style: "Lager"
